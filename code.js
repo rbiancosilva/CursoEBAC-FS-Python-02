9b0125004750bf68f19e74a94d340f5dbf58a4f5
rafinha <rbiancosilva45@gmail.com>
2024-02-20 22:09:36 -0300

    added html js and css code completed

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    alert("jquery está funcionando")
+    $('#celular').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+    $('#numero').mask('0000');
 })
