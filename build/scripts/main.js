007329c3968d3caf72a547b55fe11ab51145522e
rafinha <rbiancosilva45@gmail.com>
2024-03-27 16:46:03 -0300

    adding less files and project structure

--- build/scripts/main.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){alert("This website's code has been managed with Gulp")});
--- source/scripts/main.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    alert("This website's code has been managed with Gulp");
-})
